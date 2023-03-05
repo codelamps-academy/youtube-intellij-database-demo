8b1c82ad76ea1b6641c434b52821a5374a215d31
Andre Rizaldi Brillianto <78262588+Arzailants@users.noreply.github.com>
2023-03-05 19:23:58 +0700

    create youtube-intellij-database-demo, add README.md

---
+++ src/main/java/com/codelamps/YoutubeIntellijDatabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codelamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeIntellijDatabaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeIntellijDatabaseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codelamps/YoutubeIntellijDatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeIntellijDatabaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
